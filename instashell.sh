9b4b405a39359e8d709ca31b327db558802296be
milisaoil <123568861+milisaoil@users.noreply.github.com>
2023-01-25 22:38:14 +0600

    Update instashell.sh

--- instashell.sh
+++ instashell.sh
@@ -3,7 +3,5 @@ holder@holder
 cd instashell
 holder@holder ~/instashell $ sudo chmod +× instashell.sh
 [sudo] пароль до holder:
-holder@holder ~/instashell $ sudo ./instashell.sh
-Instagram Brute Forcer v1.5.3 Author:
-Username account:https://instagram.com/ermekbayy01?igshid=NTdlMDg3MTY=
+holder@holder ~/instashell $ sudo ./instashell.
 
